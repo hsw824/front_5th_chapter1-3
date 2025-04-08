c147942ae2cfa651a16bc4264dd9dc7fc0e3c9dd
hsw824 <ghdtjddn0824@naver.com>
2025-04-09 00:08:24 +0900

    feat: useRef 추가

--- src/@lib/hooks/useRef.ts
+++ src/@lib/hooks/useRef.ts
@@ -1,4 +1,16 @@
+import { useState } from "react";
+
 export function useRef<T>(initialValue: T): { current: T } {
   // React의 useState를 이용해서 만들어보세요.
-  return { current: initialValue };
+
+  // 내가 알고 있는 ref
+  // 1. 값을 참조함
+  // 2. 리렌더링을 유발하면 안됨
+  // callback ref는?
+
+  const [ref] = useState({
+    current: initialValue,
+  });
+
+  return ref;
 }
