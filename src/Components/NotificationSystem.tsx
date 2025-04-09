ac049262a94c9e84ac4cc8f9b3ce29ad48c8ea26
hsw824 <ghdtjddn0824@naver.com>
2025-04-10 02:06:08 +0900

    feat: 각 폴더 분리, (커스텀 memo함수에 오류 발견...)

---
+++ src/Components/NotificationSystem.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { renderLog } from "../utils/utils";
+import { useNotificationContext } from "../\bcontext/notification";
+import { NotificationContextType } from "../types/types";
+
+const NotificationSystem: React.FC = React.memo(() => {
+  renderLog("NotificationSystem rendered");
+  const { notifications, removeNotification } =
+    useNotificationContext() as NotificationContextType;
+
+  return (
+    <div className="fixed bottom-4 right-4 space-y-2">
+      {notifications.map((notification) => (
+        <div
+          key={notification.id}
+          className={`p-4 rounded shadow-lg ${
+            notification.type === "success"
+              ? "bg-green-500"
+              : notification.type === "error"
+                ? "bg-red-500"
+                : notification.type === "warning"
+                  ? "bg-yellow-500"
+                  : "bg-blue-500"
+          } text-white`}
+        >
+          {notification.message}
+          <button
+            onClick={() => removeNotification(notification.id)}
+            className="ml-4 text-white hover:text-gray-200"
+          >
+            닫기
+          </button>
+        </div>
+      ))}
+    </div>
+  );
+});
+
+export default NotificationSystem;
